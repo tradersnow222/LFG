b381cc87464179135c015d6de62b97a8a5910814
Matt Snow <87370539+tradersnow222@users.noreply.github.com>
2023-05-20 07:00:37 +0900

    Update globalVariables.sol

--- globalVariables.sol
+++ globalVariables.sol
@@ -1,8 +1,4 @@
-pragma solidity >= 0.7.0 < 0.9.0;
-
-// Exercise #1:
-// create a map of wallets with amounts
-// set up a function that can update the amount of the person call the contract - current address - msg.Sender
+pragma solidity ^0.8.0;
 
 contract LedgerBalance {
 
@@ -11,44 +7,32 @@ mapping(address => uint) balance;
 function updatesBalance(uint newBalance) public {
     balance[msg.sender] = newBalance;
 }
+
 }
 
-// Excercise #2
-// 1. create a new contract called Updated
-// 2. (if you haven't done this) copy the contract LedgerBalance above your new contract
-// 3. create a public function called updatesBalance
-// 4. instantiate the data type contract LedgerBalance to a new variable called ledgerbalance (similar to struct or enum).
-// 5. set the new variable ledgerbalance = new LedgerBalance() 
-// 6. update the ledgebalance to 30 
-// 7. deploy both contracts and up then update the ledgerbalance by 30 using the Updated contract 
+contract updated {
 
-
-contract Updated {
-    
-    function updatesBalance() public {
+    function updateBalance() public {
         LedgerBalance ledgerBalance = new LedgerBalance();
         ledgerBalance.updatesBalance(30);
     }
-    
 }
 
+//testing out some other common global variables:
 
+contract simpleStorage {
 
-// Other Global Variable Examples 
-
-contract SimpleStorage {
-    
     uint storedData;
-    
+
     function set(uint x) public {
         //storedData = x;
-        // storedData = block.difficulty;
-        // storedData = block.timestamp;
+        //storedData = block.difficulty;
+        //storedData = block.timestamp;
         storedData = block.number;
     }
-    
+
     function get() public view returns (uint) {
         return storedData;
     }
-    
+
 }
